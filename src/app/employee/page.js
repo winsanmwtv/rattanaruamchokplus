a1bab551f30498e022ce2fe97d9f6ebc804fbf85
winsanmwtv <wntv2020@gmail.com>
2025-02-15 22:55:36 +0700

    1.1.0.3B - ปุ่ม

--- src/app/employee/page.js
+++ src/app/employee/page.js
@@ -81,7 +81,7 @@ const EmployeePage = () => {
                        display: 'flex',
                        justifyContent: 'center',
                        alignItems: 'center',
-                       height: '120px',
+                       height: 'auto',
                        borderRadius: '8px',
                    }}>
                     {menu.label}
